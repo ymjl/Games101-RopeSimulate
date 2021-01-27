8bfe3d40d61a75a90072ac73dd4429726a2b8ad8
SongWanqi <378879094@qq.com>
2021-01-28 01:26:49 +0800

    1.Rope()构造函数边界问题 2. 显示欧拉模拟， 计算每个质点施加的力， 速度位置。

--- src/rope.cpp
+++ src/rope.cpp
@@ -12,56 +12,86 @@ namespace CGL {
     {
         // TODO (Part 1): Create a rope starting at `start`, ending at `end`, and containing `num_nodes` nodes.
 
-//        To Comment-in this part when you implement the constructor 
-//        for (auto &i : pinned_nodes) {
-//            masses[i]->pinned = true;
-//        }
-    
+          if(num_nodes == 0 || num_nodes == 1)
+                return;
           for (int i = 0; i < num_nodes - 1; i++)
           {
               if (i == 0)
               {
                   Mass* m1 = new Mass(start, node_mass, false);
                   masses.push_back(m1);
-                  if (i + 1 < num_nodes-1)
+                  if (i + 1 == num_nodes-1)
                   {     
-                      Mass* m2= new Mass(start + (i + 1)*(end - start) / (num_nodes - 1), node_mass, true);
-                      masses.push_back(m2);
+                      Mass* m2 = new Mass(end, node_mass, false);
                       springs.push_back(new Spring(m1, m2, k));
                   }
+                  else
+                  {
+                      Mass* m2= new Mass(start + (i + 1)*(end - start) / (num_nodes - 1), node_mass, false);
+                      masses.push_back(m2);
+                      springs.push_back(new Spring(m1, m2, k));
+                  }              
                   
-              }
-              else if (i == num_nodes - 2)
-              {
-                  Mass* m1= masses[i];
-                  Mass* m2 = new Mass(end, node_mass, false);
-                  springs.push_back(new Spring(m1, m2, k));
-              }
+              }              
               else
               {
                 Mass* m1= masses[i];
-                Mass* m2= new Mass(start + (i + 1)*(end - start) / (num_nodes - 1), node_mass, true);
+                Mass* m2= new Mass(start + (i + 1)*(end - start) / (num_nodes - 1), node_mass, false);
                 masses.push_back(m2);
                 springs.push_back(new Spring(m1, m2, k));
-              }
-              
+              }      
 
           }
           
        for (auto &i : pinned_nodes) {
            masses[i]->pinned = true;
        }
+
+
+
+//         if(num_nodes == 0 || num_nodes == 1)
+//             return;
+//         Vector2D CurrentPosition = start;
+//         //treat rope as a list
+//         Mass * p = new Mass(start,node_mass,false);
+//         masses.push_back(p);
+//         Mass * l = nullptr;
+//         Spring * presentSpring = nullptr;
+//         for(int i = 1;i <= num_nodes - 1;i++)
+//         {
+//             std::cout << i;
+//             if(i == (num_nodes -1))
+//             {
+//                 CurrentPosition = end;
+//             }
+//             else
+//             {
+//                 CurrentPosition = start + i * (end-start) / (num_nodes - 1);
+//             }
+//             l = p;
+//             p = new Mass(CurrentPosition,node_mass,false);
+//             presentSpring = new Spring(l,p,k);
+//             masses.push_back(p);
+//             springs.push_back(presentSpring);
+//         }
+// //        Comment-in this part when you implement the constructor
+//         for (auto &i : pinned_nodes) {
+//             masses[i]->pinned = true;
+//         }
     }
 
     void Rope::simulateEuler(float delta_t, Vector2D gravity)
     {
         for (auto &s : springs)
         {
-            // TODO (Part 2): Use Hooke's law to calculate the force on a node
+            //TODO (Part 2): Use Hooke's law to calculate the force on a node
             auto b = s->m2->position;
             auto a = s->m1->position;
-            auto l = b - a;
-            s->m2->forces = (-1) * s->k * l /l.norm()*(l.norm() - s->rest_length);
+            auto ab = b - a;
+
+            //每个质点施加力
+            s->m1->forces += s->k * ab /ab.norm()*(ab.norm() - s->rest_length);
+            s->m2->forces -= s->k * ab /ab.norm()*(ab.norm() - s->rest_length);
         }
 
         for (auto &m : masses)
@@ -69,8 +99,19 @@ namespace CGL {
             if (!m->pinned)
             {
                 // TODO (Part 2): Add the force due to gravity, then compute the new velocity and position
+                m->forces += gravity * m->mass;
 
+        
                 // TODO (Part 2): Add global damping
+                //添加摩擦力
+                float k_d = 0.02;
+                m->forces += - k_d * m->velocity;
+                
+                //求解加速度
+                auto a = m->forces / m->mass;
+                //计算速度位置
+                m->velocity += a * delta_t;
+                m->position += m->velocity * delta_t;
             }
 
             // Reset all forces on each mass
